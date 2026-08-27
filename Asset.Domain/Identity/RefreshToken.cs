99f4fcba7952f92831ea397c5e80ffbe603f5c29
Shaban Hassan <shabanhassan1010@gmail.com>
2026-08-27 17:43:22 +0300

    Push

--- Asset.Domain/Identity/RefreshToken.cs
+++ Asset.Domain/Identity/RefreshToken.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string UserId { get; set; } = null!;
         public string Token { get; set; } = null!;
-        public DateTime ExpiresAt { get; set; }
+        public DateTime ExpiresAt { get; set; } 
         public bool IsRevoked { get; set; }
         public DateTime CreatedAt { get; set; }
         public string? ReplacedByToken { get; set; }
